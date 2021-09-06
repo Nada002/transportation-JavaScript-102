2724d5a4696da8b997ca03e7038f21d06eefd62a
Nada002 <nada_aloyaidi@outlook.com>
2021-09-06 17:35:52 +0300

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+class Transportation{}
+
+class Car extends Transportation{}
+
+class Plane extends Transportation{}
+
+class Employee {}
+
+class Driver extends Employee{}
+
+class Pilot extends Employee{}
+
+class Reservation{}
